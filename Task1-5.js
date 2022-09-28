f644c56e104af09af6c9ee558e9b1eebbe57bf37
kwantys <114281536+kwantys@users.noreply.github.com>
2022-09-28 18:03:10 +0300

    Add files via upload

---
+++ Task1-5.js
@@ -0,0 +1,26 @@
+//Task1
+var a = 4;
+console.log("S = " + a * a);
+
+//Task2  
+a = 25;
+console.log("Десятки = " + Math.floor(a / 10) );
+console.log("Одиниці = " + a % 10 );
+
+//Task3  
+a = 4;
+var b = 3;
+console.log(a > 2 && b <= 3);
+
+//Task4
+a = 1;
+b = 3;
+var c = 4;
+console.log(a < b && b < c);
+
+//Task5
+var n = 122;
+console.log(n % 2 == 0 && n >= 100 && n < 1000 );
+
+
+
