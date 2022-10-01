ef66e92954098fa5851531ae9d8d0e602f57be32
kwantys <114281536+kwantys@users.noreply.github.com>
2022-10-01 19:47:03 +0300

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,12 @@
+var n = 0;
+if(n > 0){
+  n =  n + 1;
+}
+if(n < 0){
+  n = n - 2;
+}
+if(n == 0){
+  n = 10;
+}
+
+console.log(n);
---
+++ Task4.js
@@ -0,0 +1,20 @@
+var k = 6;
+switch(k){
+  case 1:
+    console.log("погано");
+    break;
+  case 2:
+    console.log("незадовільно");
+    break; 
+  case 3:
+    console.log("задовільно");
+    break;
+  case 4:
+    console.log("добре");
+    break; 
+  case 5:
+    console.log("відмінно");
+    break;
+  default:
+    console.log("помилка");         
+}
